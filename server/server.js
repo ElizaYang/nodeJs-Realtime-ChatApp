f8f837012e458345b6566675f63ab1cfbdd4f206
ElizaYang <32000035+ElizaYang@users.noreply.github.com>
2017-10-24 17:13:10 -0500

    update port with heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../public')
-
+const publicPath = path.join(__dirname, '../PUBLIC')
+const port = process.env.PORT || 3000;
 var app = express();//The express() function is a top-level function exported by the express module.
 
 app.use(express.static(publicPath));//static directory for express, root route is now set to your public folder dir
 
-app.listen(3000, () => {
-    console.log('Server is up on 3000');
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
 });
