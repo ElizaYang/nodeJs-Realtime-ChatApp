258809401c8b53845f1f6bd0c49941446512e017
ElizaYang <32000035+ElizaYang@users.noreply.github.com>
2017-10-24 18:09:13 -0500

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../PUBLIC')
+const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
 var app = express();//The express() function is a top-level function exported by the express module.
 
